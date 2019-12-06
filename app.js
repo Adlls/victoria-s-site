a553f52fb44520a74e21db62f25b0008b69c74a2
Adlan <adlansaidullaev@gmail.com>
2019-12-07 01:52:01 +0300

    site layout for 1 block

--- app.js
+++ app.js
@@ -47,7 +47,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/services', servicesRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
